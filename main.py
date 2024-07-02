81fc8c76b80d8a21e8f75afe36445e33174ee36b
Shayaan <shayaan.absar@gmail.com>
2024-07-02 11:54:45 +0100

    uncommit

--- main.py
+++ main.py
@@ -86,10 +86,10 @@ class Model(nn.Module):
 
 		return model_probabilities
 
-#model = nn.DataParallel(Model()).to(device)
-#t = Trainer(model, cross_entropy_loss, learning_rate, device)
-#r = t.train(epochs, batch_size, batch_accumulation, input_train, output_train, input_val, output_val)
-#save(model.state_dict(), 'model.pt')
-#artifact = wandb.Artifact(name='model', type='model')
-#artifact.add_file(local_path='model.pt')
-#r.log_artifact(artifact)
+model = nn.DataParallel(Model()).to(device)
+t = Trainer(model, cross_entropy_loss, learning_rate, device)
+r = t.train(epochs, batch_size, batch_accumulation, input_train, output_train, input_val, output_val)
+save(model.state_dict(), 'model.pt')
+artifact = wandb.Artifact(name='model', type='model')
+artifact.add_file(local_path='model.pt')
+r.log_artifact(artifact)
